7856359dfeb56ff698c29780d4e7765cd0f5ab0a
Regina Pramesti <regina.pramesti.8@gmail.com>
2023-02-25 11:00:33 +1100

    Render board based on gameBoard

--- static_home/index.js
+++ static_home/index.js
@@ -1,15 +1,26 @@
+// Important: These must be multiples of 10
 const CANVAS_HEIGHT = 480;
 const CANVAS_WIDTH = 840;
 const SQUARE_WIDTH = 70;
 const GAP = 10;
+const BOARD_PADDING_TOP = 30;
+const BOARD_PADDING_LEFT = 20;
 
 const BG_COLOR = '#c7f0d8';
 const FILL_COLOR = '#43523d';
 
-let selectedSquares = [
-  [5, 3],
-  [5, 4],
-  [6, 3],
+const tileState = {
+  invisible: 0,
+  tile: 1,
+  selected: 2,
+}
+
+const gameBoard = [
+  [1, 1, 0, 1, 1, 0, 0, 1, 1],
+  [1, 0, 0, 2, 2, 1, 1, 1, 1],
+  [1, 1, 1, 1, 2, 1, 0, 0, 1],
+  [1, 1, 0, 0, 0, 0, 1, 1, 1],
+  [1, 1, 0, 1, 1, 1, 1, 1, 1],
 ];
 
 function setup() {
@@ -26,23 +37,30 @@ function draw() {
   noStroke();
   
   // Rect placement
-  for (let i = 1; i <= 9; i++) {
-    for (let j = 1; j <= 5; j++) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 5; j++) {
       
-      // Flashing logic for selected squares
-      selectedSquares.forEach(squarePos => {
-        if (squarePos[0] === i && squarePos[1] === j) {
-          if (frameCount % 2 === 0) {
-            c = color(BG_COLOR);
-          }
-        }
-      });
+      c = getSquareColor(gameBoard[j][i]);
       
       fill(c);
-      const x = (i - 1) * SQUARE_WIDTH + i * GAP;
-      const y = (j - 1) * SQUARE_WIDTH + j * GAP;
+      const x = BOARD_PADDING_LEFT + i * SQUARE_WIDTH + (i + 1) * GAP;
+      const y = BOARD_PADDING_TOP + j * SQUARE_WIDTH + (j + 1) * GAP;
       rect(x, y, SQUARE_WIDTH, SQUARE_WIDTH);
       c = color(FILL_COLOR);
+    }
+  }
+}
+
+function getSquareColor(currentSquare) {
+  if (currentSquare === tileState.invisible) {
+    return color(BG_COLOR);
+  } else if (currentSquare === tileState.tile) {
+    return color(FILL_COLOR);
+  } else if (currentSquare === tileState.selected) {
+    if (frameCount % 2 === 0) {
+      return color(BG_COLOR);
+    } else {
+      return color(FILL_COLOR);
     }
   }
 }
